990837bd6f31314c69a1a2ccf02d88d22d16adec
Daniel Vegera <daniel.vegera@gmail.com>
2019-04-30 19:56:28 +0100

    some scripts for movement in scenes, added mothership to scene4

---
+++ StarTrekBattle/Assets/Scripts/Scene2/LookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+    public GameObject Target;
+    Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = Target.transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+    }
+}
