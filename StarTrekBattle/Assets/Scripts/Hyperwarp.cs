8926c5cdde0ba964c36a8762ac38f38e7769d655
Daniel Vegera <daniel.vegera@gmail.com>
2019-05-04 19:45:54 +0100

    hyperwarp script for scene7

---
+++ StarTrekBattle/Assets/Scripts/Hyperwarp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hyperwarp : MonoBehaviour {
+
+    public Camera maincamera;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.localScale.z < 10 && maincamera.fieldOfView < 160)
+        {
+            transform.localScale += new Vector3(0, 0, 0.1f);
+            maincamera.fieldOfView += 2;
+        }
+        else
+        {
+            transform.position += transform.forward * 10;
+        }
+
+
+	}
+}
