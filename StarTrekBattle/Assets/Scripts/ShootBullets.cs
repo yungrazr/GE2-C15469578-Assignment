5abca13e697f3149b069bbaf126bd34cd78071f7
Daniel Vegera <daniel.vegera@gmail.com>
2019-05-06 17:15:25 +0100

    voice acting sequence in scene2 done

---
+++ StarTrekBattle/Assets/Scripts/ShootBullets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullets : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public GameObject target;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ StarTrekBattle/Assets/Scripts/VoiceScene2.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceScene2 : MonoBehaviour {
+
+    public AudioClip one;
+    public AudioClip two;
+    public AudioClip three;
+    public AudioClip four;
+    public AudioClip five;
+
+    public AudioSource theme;
+
+    public FadeOutFadeIn fade;
+
+    AudioSource audioSource;
+    int currentPlaying = 1;
+
+    // Use this for initialization
+    void Start () {
+        audioSource=GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+            if(currentPlaying==1 && !audioSource.isPlaying)
+            {
+                audioSource.clip = two;
+                audioSource.Play();
+                currentPlaying = 2;
+            }
+
+            if (currentPlaying == 2 && !audioSource.isPlaying)
+            {
+                audioSource.clip = three;
+                audioSource.Play();
+                currentPlaying = 3;
+            }
+
+            if (currentPlaying == 3 && !audioSource.isPlaying )
+            {
+                audioSource.clip = four;
+                audioSource.Play();
+                currentPlaying = 4;
+            }
+
+            if (currentPlaying == 4 && !audioSource.isPlaying )
+            {
+                audioSource.clip = five;
+                audioSource.Play();
+                currentPlaying = 5;
+            }
+
+            if (currentPlaying == 5 && !audioSource.isPlaying)
+            {
+            theme.volume -= 0.01f;
+                fade.fading = false;
+
+            }
+    }
+}
